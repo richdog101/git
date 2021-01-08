e61cdf1d430dba4e815699858cd9e74e762218f8
Taylor Blau <me@ttaylorr.com>
2021-01-08 13:17:52 -0500

    pack-revindex: remove unused 'find_pack_revindex()'
    Now that no callers of 'find_pack_revindex()' remain, remove the
    function's declaration and implementation entirely.
    
    Signed-off-by: Taylor Blau <me@ttaylorr.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- pack-revindex.h
+++ pack-revindex.h
@@ -11,8 +11,6 @@ struct revindex_entry {
 int load_pack_revindex(struct packed_git *p);
 int find_revindex_position(struct packed_git *p, off_t ofs);
 
-struct revindex_entry *find_pack_revindex(struct packed_git *p, off_t ofs);
-
 int offset_to_pack_pos(struct packed_git *p, off_t ofs, uint32_t *pos);
 uint32_t pack_pos_to_index(struct packed_git *p, uint32_t pos);
 off_t pack_pos_to_offset(struct packed_git *p, uint32_t pos);
